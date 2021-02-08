0d76fae8ddd3426e47234a693432f1a64f6e874d
Gerald Bauer <gerald.bauer@gmail.com>
2021-02-08 18:47:14 +0100

    recycle

--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const BlockBasic       = require( "./block-basic" )
-const BlockProofOfWork = require( "./block-proof-of-work" )
-
-
-module.exports = { basic:       BlockBasic,
-                   proofOfWork: BlockProofOfWork }
